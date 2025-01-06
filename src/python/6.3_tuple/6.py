7fd46bab705d29e4148b2c151d81ed7bd515e991
Ivan <vanzup@mail.ru>
2025-01-06 23:22:28 +0300

    first commit

---
+++ src/python/6.3_tuple/6.py
@@ -0,0 +1,28 @@
+"""
+На вход программе подаются целые числа, записанные в одну строку через пробел.
+Необходимо их прочитать и сохранить в кортеже. Затем, в кортеже найти и
+вывести в одну строчку через пробел (по порядку) все индексы неуникальных
+(повторяющихся) значений.
+
+Sample Input:
+5 4 -3 2 4 5 10 11
+
+Sample Output:
+0 1 4 5
+"""
+
+nums = tuple(input().split())
+
+# 1-ое решение
+for i, num in enumerate(nums):
+    if nums.count(num) > 1:
+        print(i, end=' ')
+print()
+
+# 2-ое решение
+indexes = ()
+for i, num in enumerate(nums):
+    if nums.count(num) > 1:
+        indexes += (i,)
+
+print(*indexes)
