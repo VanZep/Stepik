4418a73da524296be979ce3418afce503ba93dfe
Ivan <vanzup@mail.ru>
2025-01-31 22:20:44 +0300

    add areas_of_visibility tasks

---
+++ src/python/7.functions/7.9_areas_of_visibility/1.py
@@ -0,0 +1,32 @@
+"""
+Имеется программа:
+WIDTH = int(input())
+
+
+def func1():
+    WIDTH += 1
+    return WIDTH
+
+
+print(func1())
+где читается глобальная переменная WIDTH (из входного потока)
+и функция с именем func1. Допишите в теле функции команду,
+которая бы позволяла изменять глобальную переменную WIDTH.
+
+Sample Input:
+12
+
+Sample Output:
+13
+"""
+
+WIDTH = int(input())
+
+
+def func1():
+    global WIDTH
+    WIDTH += 1
+    return WIDTH
+
+
+print(func1())
