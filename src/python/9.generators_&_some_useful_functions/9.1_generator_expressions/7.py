8fbbed62e0f61e8813317979c4d1852034d0ada1
VanZep <vanzup@mail.ru>
2025-02-13 18:23:22 +0300

    add generator_expressions tasks

--- src/python/9.generators_&_some_useful_functions/9.1_generator_expressions/7.py
+++ src/python/9.generators_&_some_useful_functions/9.1_generator_expressions/7.py
@@ -11,14 +11,17 @@ f(x) = 0.5 * pow(x, 2) - 2.0
 
 Sample Input:
 0 10
+-2 2
 
 Sample Output:
 -2.0 -2.0 -2.0 -2.0 -2.0 -2.0 -2.0 -2.0 -2.0 -2.0 -2.0 -1.99 -1.99 -1.99 -1.99
 -1.99 -1.99 -1.99 -1.98 -1.98
+0.0 -0.02 -0.04 -0.06 -0.08 -0.1 -0.12 -0.14 -0.16 -0.18 -0.2 -0.21 -0.23
+-0.25 -0.27 -0.29 -0.31 -0.33 -0.34 -0.36
 """
 
-STEP = 0.01
+STEP = 100
 a, b = map(int, input().split())
-fx = lambda x: 0.5 * pow(x * STEP, 2) - 2.0
-gen = (round(fx(x), 2) for x in range(a, int(b / STEP) + 1))
+fx = lambda x: 0.5 * pow(x / STEP, 2) - 2.0
+gen = (round(fx(x), 2) for x in range(a * STEP, b * STEP + 1))
 print(*(next(gen) for _ in range(20)))
