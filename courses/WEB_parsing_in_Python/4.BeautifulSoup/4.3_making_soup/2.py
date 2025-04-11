9ab23c8593840407a803e5888fcba86b430122c5
VanZep <vanzup@mail.ru>
2025-04-11 22:39:31 +0300

    add WEB_parsing_in_Python tasks

---
+++ courses/WEB_parsing_in_Python/4.BeautifulSoup/4.3_making_soup/2.py
@@ -0,0 +1,41 @@
+"""
+Цель:
+Написать код, который будет обрабатывать предоставленную HTML-структуру,
+представляющую собой карточку товара. Код должен находить тег <p> с классом
+card-description и извлекать из него текстовое описание товара.
+
+Задача:
+Обработать предоставленную HTML-структуру.
+Найти внутри неё тег <p> с классом card-description.
+Извлечь текстовое описание товара из найденного тега.
+"""
+
+from bs4 import BeautifulSoup
+
+html_doc = """
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Пример карточки товара</title>
+</head>
+<body>
+    <div class="card">
+        <img src="image.jpg" alt="Пример изображения товара">
+        <h2 class="card-title"> iPhone 15 </h2>
+        <p class="card-description">Аппаратной основой Apple iPhone 15 Pro Max стал 3-нанометровый чипсет A17 Pro с 6-ядерным GPU и поддержкой трассировки лучей.</p>
+        <p class="card-price">999 999 руб.</p>
+        <a href="https://example.com/product-link" class="card-link">Подробнее</a>
+    </div>
+</body>
+</html>
+"""
+
+
+def main():
+    soup = BeautifulSoup(html_doc, 'lxml')
+    p_description = soup.find('p', class_='card-description').text
+    print(p_description)
+
+
+main()
