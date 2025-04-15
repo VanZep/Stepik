34816e5b16a03b090cc2fa03a059942ea6bf3c6f
VanZep <vanzup@mail.ru>
2025-04-15 21:45:30 +0300

    add Generation_of_Python_advanced tasks

--- courses/Generation_of_Python_advanced/4.nested_lists/4.4_matrices_part_1/6.py
+++ courses/Generation_of_Python_advanced/4.nested_lists/4.4_matrices_part_1/6.py
@@ -1,6 +1,6 @@
 """
 Напишите программу, которая выводит максимальный элемент в заштрихованной
-области квадратной матрицы > img_1.png.
+области квадратной матрицы > img_2.png.
 
 Формат входных данных
 На вход программе подается натуральное число n – количество строк и столбцов в
@@ -10,7 +10,7 @@
 Программа должна вывести одно число – максимальный элемент в заштрихованной
 области квадратной матрицы.
 
-Примечание. Элементы главной диагонали также учитываются.
+Примечание. Элементы диагоналей также учитываются.
 
 Тестовые данные
 
@@ -21,7 +21,7 @@ Sample Input 1:
 1 1 6
 
 Sample Output 1:
-7
+8
 
 Sample Input 2:
 4
@@ -31,7 +31,7 @@ Sample Input 2:
 0 0 0 0
 
 Sample Output 2:
-0
+7
 
 Sample Input 3:
 2
@@ -40,32 +40,10 @@ Sample Input 3:
 
 Sample Output 3:
 9
-
-Sample Input 4:
-3
--50 -10 -20
--19 -78 -70
--11 -12 -19
-
-Sample Output 4:
--11
 """
 
 from sys import maxsize
 
-# 1-ое решение
-n = int(input())
-matrix = [list(map(int, input().split())) for _ in range(n)]
-
-max_num = -maxsize - 1
-
-for i in range(n):
-    for j in range(i + 1):
-        max_num = max(max_num, matrix[i][j])
-
-print(max_num)
-
-# 2-ое решение
 n = int(input())
 matrix = [list(map(int, input().split())) for _ in range(n)]
 
@@ -73,7 +51,7 @@ max_num = -maxsize - 1
 
 for i in range(n):
     for j in range(n):
-        if i >= j:
+        if i >= j and i <= n - 1 - j or i <= j and i >= n - 1 - j:
             max_num = max(max_num, matrix[i][j])
 
 print(max_num)
