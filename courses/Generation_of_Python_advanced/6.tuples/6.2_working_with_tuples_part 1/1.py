16b89a74d04ce41c5eac6d0c706ac5b3b86e255f
Ivan <vanzup@mail.ru>
2025-05-07 19:52:45 +0300

    add Generation_of_Python_advanced tasks

---
+++ courses/Generation_of_Python_advanced/6.tuples/6.2_working_with_tuples_part 1/1.py
@@ -0,0 +1,10 @@
+"""
+Используя индексацию кортежа, дополните приведенный ниже код так, чтобы
+переменная last содержала последний элемент кортежа countries
+"""
+
+countries = (
+    'Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy'
+)
+last = countries[-1]
+print(last)
