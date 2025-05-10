ae56a3d7067c2e9ea800b16ec7732fb0af0b1651
Ivan <vanzup@mail.ru>
2025-05-10 22:19:47 +0300

    add Generation_of_Python_advanced tasks

---
+++ courses/Generation_of_Python_advanced/6.tuples/6.2_working_with_tuples_part 1/3.py
@@ -0,0 +1,15 @@
+"""
+Используя срезы, дополните приведенный ниже код так, чтобы он вывел элементы
+кортежа countries кроме первых двух:
+countries = (
+    'Russia', 'Argentina', 'Slovakia', 'Canada', 'Slovenia',
+    'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon'
+)
+"""
+
+countries = (
+    'Russia', 'Argentina', 'Slovakia', 'Canada', 'Slovenia',
+    'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon'
+)
+
+print(countries[2:])
