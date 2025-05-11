e266af3eb403ae9ceefd7f3a0ef960129a8d6719
Ivan <vanzup@mail.ru>
2025-05-11 13:21:19 +0300

    add Generation_of_Python_advanced tasks

---
+++ courses/Generation_of_Python_advanced/6.tuples/6.2_working_with_tuples_part 1/6.py
@@ -0,0 +1,17 @@
+"""
+Дополните приведенный ниже код так, чтобы переменная number содержала
+количество элементов кортежа countries:
+countries = (
+    'Romania', 'Poland', 'Estonia', 'Bulgaria',
+    'Slovakia', 'Slovenia', 'Hungary'
+)
+number =
+"""
+
+countries = (
+    'Romania', 'Poland', 'Estonia', 'Bulgaria',
+    'Slovakia', 'Slovenia', 'Hungary'
+)
+number = len(countries)
+
+print(number)
