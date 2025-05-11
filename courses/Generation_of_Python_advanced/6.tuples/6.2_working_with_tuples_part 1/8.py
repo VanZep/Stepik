9bc447800a5fbd6a3de8bc1bbc18fcb614ce26ac
Ivan <vanzup@mail.ru>
2025-05-11 13:40:54 +0300

    add Generation_of_Python_advanced tasks

---
+++ courses/Generation_of_Python_advanced/6.tuples/6.2_working_with_tuples_part 1/8.py
@@ -0,0 +1,15 @@
+"""
+Дополните приведенный ниже код так, чтобы переменная index содержала индекс
+элемента 'Slovenia' в кортеже countries:
+countries = (
+    'Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy'
+)
+index =
+"""
+
+countries = (
+    'Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy'
+)
+index = countries.index('Slovenia')
+
+print(index)
---
+++ courses/Generation_of_Python_advanced/6.tuples/6.2_working_with_tuples_part 1/9.py
@@ -0,0 +1,17 @@
+"""
+Дополните приведенный ниже код так, чтобы переменная number содержала
+количество вхождений 'Spain' в кортеж countries:
+countries = (
+    'Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia',
+    'Italy', 'Spain', 'Ukraine', 'Chile', 'Spain', 'Cameroon'
+)
+number =
+"""
+
+countries = (
+    'Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia',
+    'Italy', 'Spain', 'Ukraine', 'Chile', 'Spain', 'Cameroon'
+)
+number = countries.count('Spain')
+
+print(number)
