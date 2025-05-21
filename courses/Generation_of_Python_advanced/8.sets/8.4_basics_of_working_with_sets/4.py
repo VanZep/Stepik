3ce73b60f1fb52c46bc6eed761678569afcc7037
Ivan <vanzup@mail.ru>
2025-05-21 23:47:41 +0300

    add Generation_of_Python_advanced tasks

---
+++ courses/Generation_of_Python_advanced/8.sets/8.4_basics_of_working_with_sets/4.py
@@ -0,0 +1,16 @@
+"""
+Дополните приведенный код, чтобы он вывел элементы множества fruits, каждый на
+отдельной строке, отсортированные по убыванию (в обратном лексикографическом
+порядке):
+fruits = {
+    'apple', 'banana', 'cherry', 'avocado', 'pineapple',
+    'apricot', 'banana', 'avocado', 'grapefruit'
+}
+"""
+
+fruits = {
+    'apple', 'banana', 'cherry', 'avocado', 'pineapple',
+    'apricot', 'banana', 'avocado', 'grapefruit'
+}
+
+print(*sorted(fruits, reverse=True), sep='\n')
