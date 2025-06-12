3c2d5b98943f4de84e038893312df48fb7cd2c9a
Ivan <vanzup@mail.ru>
2025-06-12 16:43:17 +0300

    add Generation_of_Python_advanced tasks

---
+++ courses/Generation_of_Python_advanced/10.dicts/10.3_dictionary_methods/4.py
@@ -0,0 +1,25 @@
+"""
+Дополните приведенный ниже код так, чтобы он вывел наиболее часто
+встречающееся слово строки s. Если таких слов несколько, должно быть выведено
+то, что меньше в лексикографическом порядке.
+"""
+
+s = (
+    'orange strawberry barley gooseberry apple apricot barley currant orange'
+    'melon pomegranate banana banana orange barley apricot plum grapefruit'
+    'bananaquince strawberry barley grapefruit banana grapes melon strawberry'
+    'apricot currant currant gooseberry raspberry apricot currant orange lime'
+    'quince grapefruit barley banana melon pomegranate barley banana orange'
+    'barley apricot plum banana quince lime grapefruit strawberry gooseberry'
+    'apple barley apricot currant orange melon pomegranate banana banana'
+    'orangeapricot barley plum banana grapefruit banana quince currant orange'
+    'melonpomegranate barley plum banana quince barley lime grapefruit'
+    'pomegranate barley'
+)
+
+word_count = {word: s.count(word) for word in s.split()}
+print(word_count)
+
+print(min(
+    word for word in word_count if word_count[word] == max(word_count.values())
+))
