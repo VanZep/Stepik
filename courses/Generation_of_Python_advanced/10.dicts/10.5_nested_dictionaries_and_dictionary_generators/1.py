db36d30365141397bf0b3df2c51ab82bc5bae2e2
Ivan <vanzup@mail.ru>
2025-06-17 19:42:49 +0300

    add Generation_of_Python_advanced tasks

--- courses/Generation_of_Python_advanced/10.dicts/10.5_nested_dictionaries_and_dictionary_generators/1.py
+++ courses/Generation_of_Python_advanced/10.dicts/10.5_nested_dictionaries_and_dictionary_generators/1.py
@@ -8,7 +8,6 @@ numbers = [
     34, 10, -4, 6, 10, 23, -90, 100, 21, -35, -95, 1, 36, -38, -19, 1, 6, 87
 ]
 
-# result = {i: num ** 2 for i, num in enumerate(numbers)}
-result = {i: numbers[i] ** 2 for i in range(len(numbers))}
-# print(numbers.index(10))
+result = {i: num ** 2 for i, num in enumerate(numbers)}
+
 print(result)
