deaec3b3390c5b86cb7fc390cd7b895c3d8a1bae
VanZep <vanzup@mail.ru>
2025-06-18 22:35:14 +0300

    add Generation_of_Python_advanced tasks

---
+++ courses/Generation_of_Python_advanced/12.random_and_string_modules/3.py
@@ -0,0 +1,16 @@
+"""
+Напишите программу, которая с помощью модуля random генерирует случайный
+пароль. Программа принимает на вход длину пароля и выводит случайный пароль,
+содержащий только символы английского алфавита a..z, A..Z
+(в нижнем и верхнем регистре).
+"""
+
+import string
+import random
+
+length = int(input())  # длина пароля
+
+password = ''.join(
+    [random.choice(string.ascii_letters) for _ in range(length)]
+)
+print(password)
