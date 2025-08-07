e52f336da92ec79e41a1de249c9c7476b2da61d8
VanZep <vanzup@mail.ru>
2025-08-07 19:16:06 +0300

    create auth on JWT

--- projects/fastapi_ecommerce/app/backend/db_depends.py
+++ projects/fastapi_ecommerce/app/backend/db_depends.py
@@ -2,7 +2,7 @@ from typing import AsyncGenerator
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from backend.db import SessionLocal, async_session_maker
+from app.backend.db import SessionLocal, async_session_maker
 
 
 # async def get_db():
