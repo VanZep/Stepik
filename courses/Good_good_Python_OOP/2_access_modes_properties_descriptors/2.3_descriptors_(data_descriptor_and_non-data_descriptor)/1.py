a0f8f139d49d357f0ab3be781d66276f2d80cd6d
VanZep <vanzup@mail.ru>
2025-09-28 13:25:55 +0300

    add 2.3_descriptors_(data_descriptor_and_non-data_descriptor) tasks

--- courses/Good_good_Python_OOP/2_access_modes_properties_descriptors/2.3_descriptors_(data_descriptor_and_non-data_descriptor)/1.py
+++ courses/Good_good_Python_OOP/2_access_modes_properties_descriptors/2.3_descriptors_(data_descriptor_and_non-data_descriptor)/1.py
@@ -1,35 +1,74 @@
-class StringField:
+"""
+Объявите дескриптор данных FloatValue, который бы устанавливал и возвращал
+вещественные значения. При записи вещественного числа должна выполняться
+проверка на вещественный тип данных. Если проверка не проходит, то
+генерировать исключение командой:
+raise TypeError("Присваивать можно только вещественный тип данных.")
+
+Объявите класс Cell, в котором создается объект value дескриптора FloatValue.
+А объекты класса Cell должны создаваться командой:
+cell = Cell(начальное значение ячейки)
+
+Объявите класс TableSheet, с помощью которого создается таблица из N строк и
+M столбцов следующим образом:
+table = TableSheet(N, M)
+
+Каждая ячейка этой таблицы должна быть представлена объектом класса Cell,
+работать с вещественными числами через объект value (начальное значение должно
+быть 0.0).
+
+В каждом объекте класса TableSheet должен формироваться локальный атрибут:
+cells - список (вложенный) размером N x M, содержащий ячейки таблицы (объекты
+класса Cell).
+
+Создайте объект table класса TableSheet с размером таблицы N = 5, M = 3.
+Запишите в эту таблицу числа от 1.0 до 15.0 (по порядку, построчно).
+
+P.S. На экран в программе выводить ничего не нужно.
+"""
+
+
+class FloatValue:
+
     def __set_name__(self, owner, name):
-        print('__set_name__', name, owner.__name__, self.__dict__)
-        self.name = "_" + name
-        print(self.__dict__, self.__class__)
+        self.name = '_' + name
 
     def __get__(self, instance, owner):
-        print('__get__')
-        # return instance.__dict__[self.name]
         return getattr(instance, self.name)
 
     def __set__(self, instance, value):
-        print('__set__', instance.__class__, instance.__dict__, self.__class__, self.__dict__)
-        # instance.__dict__[self.name] = value
+        self.verify_float(value)
         setattr(instance, self.name, value)
-        print(instance.__dict__, value)
+
+    @staticmethod
+    def verify_float(value):
+        if not isinstance(value, float):
+            raise TypeError(
+                'Присваивать можно только вещественный тип данных.'
+            )
 
 
-class DataBase:
-    print('x = StringField()')
-    x = StringField()
-    # y = StringField()
+class Cell:
+    value = FloatValue()
 
-    def __init__(self, x, y):
-        print('self.x = x')
-        self.x = x
-        # self.y = y
+    def __init__(self, value=0.0):
+        self.value = value
 
 
-db = DataBase('hi', 'low')
-print(db.__dict__)
-db.__dict__['x'] = 'top'
-print(db.__dict__)
-print(db.x)
-# print(db.y)
+class TableSheet:
+
+    def __init__(self, width, height):
+        self.cells = [[Cell() for _ in range(height)] for _ in range(width)]
+
+    def fill_table(self, start):
+        for row in self.cells:
+            for cell in row:
+                cell.value = float(start)
+                start += 1
+
+
+table = TableSheet(5, 3)
+table.fill_table(1)
+for row in table.cells:
+    for cell in row:
+        print(cell.value)
