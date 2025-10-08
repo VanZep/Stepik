25dc88b9d41958f0418af85db2abad0e095df854
Ivan <vanzup@mail.ru>
2025-10-08 17:21:25 +0300

    add Good_good_Python_OOP tasks

---
+++ courses/Good_good_Python_OOP/3.magical_class_methods/3.1_methods__setattr__,__getattribute__,__getattr__and__delattr__/6.py
@@ -0,0 +1,94 @@
+"""
+Объявите класс Circle (окружность), объекты которого должны создаваться
+командой:
+circle = Circle(x, y, radius)
+где x, y - координаты центра окружности; radius - радиус окружности
+
+В каждом объекте класса Circle должны формироваться локальные приватные
+атрибуты:
+__x, __y - координаты центра окружности (вещественные или целые числа)
+__radius - радиус окружности (вещественное или целое положительное число)
+
+Для доступа к этим приватным атрибутам в классе Circle следует объявить
+объекты-свойства (property):
+x, y - для изменения и доступа к значениям __x, __y, соответственно
+radius - для изменения и доступа к значению __radius
+
+При изменении значений приватных атрибутов через объекты-свойства нужно
+проверять, что присваиваемые значения - числа (целые или вещественные).
+Дополнительно у радиуса проверять, что число должно быть положительным
+(строго больше нуля). Сделать все эти проверки нужно через магические методы.
+При некорректных переданных числовых значениях, прежние значения меняться не
+должны (исключений никаких генерировать при этом не нужно).
+
+Если присваиваемое значение не числовое, то генерировать исключение командой:
+raise TypeError("Неверный тип присваиваемых данных.")
+
+При обращении к несуществующему атрибуту объектов класса Circle выдавать
+булево значение False.
+
+Пример использования класса (эти строчки в программе писать не нужно):
+circle = Circle(10.5, 7, 22)
+circle.radius = -10 # прежнее значение не должно меняться, т.к. отрицательный
+радиус недопустим
+x, y = circle.x, circle.y
+res = circle.name # False, т.к. атрибут name не существует
+
+
+P.S. На экран ничего выводить не нужно.
+"""
+
+
+class Circle:
+    __x: (int, float)
+    __y: (int, float)
+    __radius: (int, float)
+
+    def __init__(self, x, y, radius):
+        self.__x = x
+        self.__y = y
+        self.__radius = radius
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+    @property
+    def radius(self):
+        return self.__radius
+
+    @radius.setter
+    def radius(self, value):
+        self.__radius = value
+
+    def __setattr__(self, key, value):
+        if key in self.__annotations__ and (
+            not isinstance(value, self.__annotations__.get(key))
+        ):
+            raise TypeError('Неверный тип присваиваемых данных.')
+        if key == 'radius' and value <= 0:
+            return
+        super().__setattr__(key, value)
+
+    def __getattr__(self, item):
+        return False
+
+
+circle = Circle(10.5, 7, 22)
+circle.radius = -10  # прежнее значение не должно меняться, т.к. отрицательный радиус недопустим
+x, y = circle.x, circle.y
+print(x, y)
+res = circle.name  # False, т.к. атрибут name не существует
+print(res)
