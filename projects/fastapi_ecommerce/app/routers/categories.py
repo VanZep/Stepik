3f96aa1c2f07e8c83586841a92f7ba6729574e43
VanZep <vanzup@mail.ru>
2025-11-10 17:37:52 +0300

    create update_category endpoint

--- projects/fastapi_ecommerce/app/routers/categories.py
+++ projects/fastapi_ecommerce/app/routers/categories.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from fastapi import APIRouter, status, Depends, HTTPException
-from sqlalchemy import select, and_
+from sqlalchemy import select, update, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.categories import Category as CategoryModel
@@ -60,6 +60,72 @@ async def create_category(
 
     db_category = CategoryModel(**category.model_dump())
     db.add(db_category)
+    await db.commit()
+    await db.refresh(db_category)
+
+    return db_category
+
+
+@router.put(
+    "/{category_id}",
+    response_model=CategorySchema,
+    status_code=status.HTTP_200_OK
+)
+async def update_category(
+        category_id: int,
+        category: CategoryCreate,
+        db: AsyncSession = Depends(get_async_db)
+):
+    """
+    Обновляет категорию по её ID.
+    """
+    db_category = await db.scalars(
+        select(
+            CategoryModel
+        ).where(
+            and_(
+                CategoryModel.id == category_id,
+                CategoryModel.is_active == True
+            )
+        )
+    )
+    db_category = db_category.first()
+    if db_category is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Category not found or not active"
+        )
+
+    if category.parent_id:
+        parent = await db.scalars(
+            select(
+                CategoryModel
+            ).where(
+                and_(
+                    CategoryModel.id == category.parent_id,
+                    CategoryModel.is_active == True
+                )
+            )
+        )
+        parent = parent.first()
+        if parent is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Category parent not found or not active"
+            )
+        if category_id == parent.id:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Category cannot be its own parent"
+            )
+
+    await db.execute(
+        update(
+            CategoryModel
+        ).where(
+            CategoryModel.id == category_id
+        ).values(**category.model_dump(exclude_unset=True))
+    )
     await db.commit()
     await db.refresh(db_category)
 
