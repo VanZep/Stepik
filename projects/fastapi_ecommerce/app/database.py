113eb3faefdfd4af66833224e65558c4253e1cd1
VanZep <vanzup@mail.ru>
2025-11-10 12:17:44 +0300

    create async_session_maker

--- projects/fastapi_ecommerce/app/database.py
+++ projects/fastapi_ecommerce/app/database.py
@@ -1,9 +1,31 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.ext.asyncio import (
+    create_async_engine, async_sessionmaker, AsyncSession
+)
 
+# Строка подключения для SQLite
 DATABASE_URL = "sqlite:///ecommerce.db"
+
+# Создаём Engine
 engine = create_engine(DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов
 SessionLocal = sessionmaker(bind=engine)
+
+
+# Строка подключения для PostgreSQl
+DATABASE_URL = "postgresql+asyncpg://ecommerce_user:xxxxxxxx@localhost:5432/ecommerce_db"
+
+# Создаём Engine
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов
+async_session_maker = async_sessionmaker(
+    async_engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
 
 
 class Base(DeclarativeBase):
