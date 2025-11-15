b78be188d4fd6aa3f2abeeea3719019ca1af14c9
VanZep <vanzup@mail.ru>
2025-11-15 11:22:21 +0300

    create User model

---
+++ projects/fastapi_ecommerce/app/models/users.py
@@ -0,0 +1,43 @@
+from typing import List
+
+from sqlalchemy import Integer, String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        nullable=False,
+        unique=True
+    )
+    email: Mapped[str] = mapped_column(
+        String,
+        unique=True,
+        index=True,
+        nullable=False
+    )
+    hashed_password: Mapped[str] = mapped_column(
+        String,
+        nullable=False
+    )
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True
+    )
+    role: Mapped[str] = mapped_column(
+        String,
+        default='buyer'
+    )
+
+    products: Mapped[List['Product']] = relationship(
+        'Product',
+        uselist=True,
+        back_populates='seller',
+        cascade='all, delete-orphan'
+    )
