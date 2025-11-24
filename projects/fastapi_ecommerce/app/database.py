de864c4ce80613c081e54e8e7870c0c0423fbd5e
VanZep <vanzup@mail.ru>
2025-11-24 19:42:27 +0300

    add naming_convention in Base model

--- projects/fastapi_ecommerce/app/database.py
+++ projects/fastapi_ecommerce/app/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from sqlalchemy.ext.asyncio import (
     create_async_engine, async_sessionmaker, AsyncSession
@@ -29,4 +29,13 @@ async_session_maker = async_sessionmaker(
 
 
 class Base(DeclarativeBase):
-    pass
+
+    metadata = MetaData(
+        naming_convention={
+            'ix': 'ix_%(column_0_label)s',
+            'uq': 'uq_%(table_name)s_%(column_0_N_name)s',
+            'ck': 'ck_%(table_name)s_%(constraint_name)s',
+            'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+            'pk': 'pk_%(table_name)s'
+        }
+    )
