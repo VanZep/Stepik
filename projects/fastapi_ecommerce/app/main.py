e2c8a46be78f15e61dad072896d4df3e729c5348
VanZep <vanzup@mail.ru>
2025-12-07 18:11:29 +0300

    create media folder

--- projects/fastapi_ecommerce/app/main.py
+++ projects/fastapi_ecommerce/app/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.routers import categories, products, users, reviews, cart, orders
 
@@ -7,6 +8,7 @@ app = FastAPI(
     title="FastAPI Интернет-магазин",
     version="0.1.0",
 )
+app.mount("/media", StaticFiles(directory="media"), name="media")
 
 app.include_router(categories.router)
 app.include_router(products.router)
