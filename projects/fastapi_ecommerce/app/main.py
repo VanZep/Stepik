a09253c8e4a787b536745ddbe6fa49e1dfd1a0d6
VanZep <vanzup@mail.ru>
2025-12-13 22:51:17 +0300

    add logger

--- projects/fastapi_ecommerce/app/main.py
+++ projects/fastapi_ecommerce/app/main.py
@@ -1,9 +1,19 @@
 import uvicorn
-from fastapi import FastAPI
+from uuid import uuid4
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
+from loguru import logger
 
 from app.routers import categories, products, users, reviews, cart, orders
 from app.config import MEDIA_ROOT
+
+logger.add(
+    "info.log",
+    format="Log: [{extra[log_id]}:{time} - {level} - {message}]",
+    level="INFO",
+    enqueue = True
+)
 
 app = FastAPI(
     title="FastAPI Интернет-магазин",
@@ -14,6 +24,29 @@ app.mount(
     StaticFiles(directory=MEDIA_ROOT),
     name="media"
 )
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [
+                status.HTTP_400_BAD_REQUEST,
+                status.HTTP_402_PAYMENT_REQUIRED,
+                status.HTTP_403_FORBIDDEN,
+                status.HTTP_404_NOT_FOUND
+            ]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(
+                content={"success": False},
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+        return response
 
 app.include_router(categories.router)
 app.include_router(products.router)
@@ -31,5 +64,5 @@ async def root():
     return {"message": "Добро пожаловать в API интернет-магазина!"}
 
 
-if __name__ == '__main__':
-    uvicorn.run('app.main:app')
+# if __name__ == '__main__':
+#     uvicorn.run('app.main:app')
