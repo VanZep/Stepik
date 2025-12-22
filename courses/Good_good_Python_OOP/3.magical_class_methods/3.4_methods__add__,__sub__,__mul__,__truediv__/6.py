fa42e6bd8d4cefc043dc917439c39347557daa71
VanZep <vanzup@mail.ru>
2025-12-22 12:49:37 +0300

    add Good_good_Python_OOP tasks

---
+++ courses/Good_good_Python_OOP/3.magical_class_methods/3.4_methods__add__,__sub__,__mul__,__truediv__/6.py
@@ -0,0 +1,122 @@
+"""
+Объявите класс Box3D для представления прямоугольного параллелепипеда (бруска),
+объекты которого создаются командой:
+box = Box3D(width, height, depth)
+где width, height, depth - ширина, высота и глубина соответственно
+(числа: целые или вещественные)
+
+В каждом объекте класса Box3D должны создаваться публичные атрибуты:
+width, height, depth - ширина, высота и глубина соответственно.
+
+С объектами класса Box3D должны выполняться следующие операторы:
+box1 = Box3D(1, 2, 3)
+box2 = Box3D(2, 4, 6)
+
+box = box1 + box2 # Box3D: width=3, height=6, depth=9 (соответствующие
+размерности складываются)
+box = box1 * 2    # Box3D: width=2, height=4, depth=6 (каждая размерность
+умножается на 2)
+box = 3 * box2    # Box3D: width=6, height=12, depth=18
+box = box2 - box1 # Box3D: width=1, height=2, depth=3 (соответствующие
+размерности вычитаются)
+box = box1 // 2   # Box3D: width=0, height=1, depth=1 (соответствующие
+размерности целочисленно делятся на 2)
+box = box2 % 3    # Box3D: width=2, height=1, depth=0
+
+При каждой арифметической операции следует создавать новый объект класса Box3D
+с соответствующими значениями локальных атрибутов.
+
+
+P.S. В программе достаточно только объявить класс Box3D. На экран ничего
+выводить не нужно.
+"""
+
+from typing import Union, Optional
+
+
+class Box3D:
+
+    def __init__(
+            self,
+            width: Union[int, float],
+            height: Union[int, float],
+            depth: Union[int, float]
+    ) -> None:
+        self.width = width
+        self.height = height
+        self.depth = depth
+
+    def __add__(self, other: 'Box3D') -> Optional['Box3D']:
+        if isinstance(other, Box3D):
+            return Box3D(
+                self.width + other.width,
+                self.height + other.height,
+                self.depth + other.depth
+            )
+        raise ArithmeticError('Складывать можно только объекты класса Box3D')
+
+    def __mul__(self, other: int) -> 'Box3D':
+        if type(other) == int:
+            return Box3D(
+                self.width * other,
+                self.height * other,
+                self.depth * other
+            )
+        raise ArithmeticError(
+            'Эту операцию можно выполнять только с целым числом слева'
+        )
+
+    def __rmul__(self, other: int) -> 'Box3D':
+        if type(other) == int:
+            return self.__mul__(other)
+        raise ArithmeticError(
+            'Эту операцию можно выполнять только с целым числом справа'
+        )
+
+    def __sub__(self, other: 'Box3D') -> Optional['Box3D']:
+        if isinstance(other, Box3D):
+            return Box3D(
+                self.width - other.width,
+                self.height - other.height,
+                self.depth - other.depth
+            )
+        raise ArithmeticError('Вычитать можно только объекты класса Box3D')
+
+    def __floordiv__(self, other: int) -> 'Box3D':
+        if type(other) == int:
+            return Box3D(
+                self.width // other,
+                self.height // other,
+                self.depth // other
+            )
+        raise ArithmeticError(
+            'Эту операцию можно выполнять только с целым числом слева'
+        )
+
+    def __mod__(self, other: int) -> 'Box3D':
+        if type(other) == int:
+            return Box3D(
+                self.width % other,
+                self.height % other,
+                self.depth % other
+            )
+        raise ArithmeticError(
+            'Эту операцию можно выполнять только с целым числом слева'
+        )
+
+
+box1 = Box3D(1, 2, 3)
+box2 = Box3D(2, 4, 6)
+
+box = box1 + box2  # Box3D: width=3, height=6, depth=9 (соответствующие размерности складываются)
+print(box.width, box.height, box.depth)
+box = box1 * 2  # Box3D: width=2, height=4, depth=6 (каждая размерность умножается на 2)
+print(box.width, box.height, box.depth)
+box = 3 * box2  # Box3D: width=6, height=12, depth=18
+print(box.width, box.height, box.depth)
+box = box2 - box1  # Box3D: width=1, height=2, depth=3 (соответствующие размерности вычитаются)
+print(box.width, box.height, box.depth)
+box = box1 // 2  # Box3D: width=0, height=1, depth=1 (соответствующие размерности целочисленно делятся на 2)
+print(box.width, box.height, box.depth)
+box = box2 % 3  # Box3D: width=2, height=1, depth=0
+print(box.width, box.height, box.depth)
